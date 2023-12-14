a01d7b58997426560ce3b16231aad92b86e2fe71
HariEashwar <hari.eash03@gmail.com>
2023-12-14 15:38:16 +0530

    modified

--- backend/myproject/myapp2/views.py
+++ backend/myproject/myapp2/views.py
@@ -2,14 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+import pandas as pd
+df=pd.read_csv("m.land_dataset.csv")
+
 class search(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
         input_value = request.data.get('input')
         place = input_value
-
-        
+        df2=df[df["District"]==place]
+        polygon=df2["polygon"]
 
 
         return Response({'data': input_value**2})
