c36d61a432ac5af7a828ea7891923c6779006b3e
Dharun S <sdharun2211@gmail.com>
2023-12-19 11:25:14 +0530

    fixing issues with usestate

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,52 +1,54 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-// import Map from "./Map";
 
 function Main() {
+    const navigate = useNavigate()
     const [district, setDistrict] = useState('');
-    const [responseData, setResponseData] = useState(null);
-  
+
     const handleInputChange = (event) => {
-      setDistrict(event.target.value);
+        setDistrict(event.target.value);
     };
-  
+
     const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/search/', {
-          params: { input: district }
-        });
-  
-        if (response.status === 200) {
-          setResponseData(response.data);
-        } else {
-          console.error('Failed to retrieve data. Status code:', response.status);
+        try {
+            const response = await axios.get('http://localhost:8000/search/', {
+                params: { input: district }
+            });
+    
+            if (response.status === 200) {
+                console.log('Response data:', response.data);
+                navigate('/map', { state: { polygonData: response.data } });
+                console.log(response.data.data.polygon);
+            } else {
+                console.error('Failed to retrieve data. Status code:', response.status);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
         }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
     };
-    return(
+
+    return (
         <div className="main">
-             <div className="background">
+            <div className="background">
                 <div className="glass">
                     <div className="text">
-                        Lets Analyse the place you like
+                        Let's Analyze the place you like
                     </div>
                 </div>
                 <div className="search-container">
                     <div className="search-box">
-                            Place Name : 
-                            <input type="text" name="search" placeholder="Search" onChange={handleInputChange}/>
+                        Place Name :
+                        <input type="text" name="search" placeholder="Search" onChange={handleInputChange} />
                     </div>
                     <button className="search-btn" onClick={fetchData}>
                         Search
                     </button>
                 </div>
             </div>
-            {responseData && (
-          <pre>{JSON.stringify(responseData, null, 2)}</pre>
-        )}
+            {/* <Map polygonData={responseData} /> */}
         </div>
-    )
+    );
 }
+
 export default Main;
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,13 +1,34 @@
-// import React, { useState, useEffect } from 'react';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useLocation } from "react-router-dom";
+
+function Map() {
+    const location = useLocation();
+    let polygonData;
+    console.log('Location state:', location.state.polygonData.data.polygon);
+    if (location.state) {
+        polygonData = location.state.polygonData.data.polygon;
+    } else {
+        polygonData = null;
+    }
 
 
-function Map({data}) {
+    const position = [51.505, -0.09]
+
     return (
-        <div>
-        <h1>Result Page</h1>
-        {/* <p>Result:</p> */}
-        </div>
-    );
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    )
 }
 
 export default Map;
